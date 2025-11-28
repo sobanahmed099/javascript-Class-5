c0c780e418c8fbfeb627db0b32ae3af1e165f1e7
sobanahmed099 <sobanahmedkigs125@gmail.com>
2025-11-28 14:19:19 +0500

    Add files via upload

---
+++ Javascript Class 5 Asiignment 16/app.js
@@ -0,0 +1,67 @@
+// let arr=[
+
+//     [ ], 
+//     [ ],
+//     [ ]
+// ];
+
+
+
+// arr[0][0]=  ` 0,1,2,3 `
+// arr[1][0]= ` 1,0,1,2 `
+// arr[2][0]= ` 2,1,0,1 `
+// console.log(arr[0][0]);
+// console.log(arr[1][0]);
+// console.log(arr[2][0]);
+
+
+
+// for(let i = 0; i <= 10; i++){
+//     console.log(i);
+// }
+
+
+
+
+// let num = +prompt("Enter a num and its table will appear on your sceen")
+// let length = +prompt("Enter length of table ")
+// for(let i = 1; i <= length; i++){
+//     document.write(`${num} x ${i}  =  ${num*i} <br>`)
+//  }
+
+
+
+// let fruits = ['apple', 'banana', 'mango', 'orange','strawberry']
+// fo(let i= fruits ; i= ; 1++){
+
+// }
+// console.log(fruits[0]);
+// console.log(fruits[1]);
+// console.log(fruits[2]);
+// console.log(fruits[3]);
+// console.log(fruits[4]);
+// console.log("Element at index 0 is " + fruits[0]);
+// console.log("Element at index 1 is " + fruits[1]);
+// console.log("Element at index 2 is " + fruits[2]);
+// console.log("Element at index 3 is " + fruits[3]);
+// console.log("Element at index 4 is " + fruits[4]);
+
+
+
+
+
+document.write("Counting: <br>")
+for(let i = 1; i<=15; i++){
+    document.write(i+",");
+}
+document.write("<br><br> Reverse Counting: <br>")
+
+
+
+
+
+
+
+
+
+
